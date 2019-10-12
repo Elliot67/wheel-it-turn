a7881c7198e7ae915c31bd644658469f20078728
Elliot Lintz <elliot.lintz@gmail.com>
2019-10-13 01:07:19 +0200

    Amélioration gestion des tabs & items

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,4 @@
+// Footer
 gitHubLink.addEventListener('click', () => {
     chrome.tabs.create({ url: "https://github.com/Elliot67/HigherOrLower" });
 });
@@ -6,43 +7,80 @@ gitHubLink.addEventListener('click', () => {
 // The extension
 
 class Tab {
-    constructor(id, name, color) {
-        this.id = 'tab_' + id;
+    constructor(name, color) {
+        this.tabId;
         this.name = name;
         this.color = color;
+        this.itemsId = [];
         this.items = [];
-        this.create();
+        this.createTabId();
+        this.createElement();
     }
 
-    create() {
-        main.append('<p id="' + this.id + '">' + 'Roulette !' + '</p>');
+    createTabId() {
+        let id;
+        do {
+            id = Math.floor(Math.random() * MAX_TABS);
+        } while (tabsId.includes(id))
+        console.log('tab'+id);
+        this.tabId = id;
+        tabsId.push(this.tabId);
+    }
+
+    createElement() {
+        tabSection.insertAdjacentHTML('beforeend', '<div style="background-color: '+ this.color +'" data-tabId="' + this.tabId + '">' + this.name + '</div>');
     }
 
     addItem(name, color) {
-        new Item(name, color);
+        this.items.push(new Item(this, name, color));
     }
-
 }
 
 
 class Item {
-    constructor(name, color, id) {
+    constructor(tab, name, color) {
+        console.log(tab);
+        this.tab = tab;
+        this.itemId;
         this.name = name;
         this.color = color;
+        this.createItemdId();
+        this.createElement();
     }
 
+    createItemdId() {
+        let id;
+        do {
+            id = Math.floor(Math.random() * MAX_ITEMS);
+        } while (this.tab.itemsId.includes(id))
+        console.log('item: '+id);
+        this.itemId = id;
+        this.tab.itemsId.push(this.itemId);
+    }
+
+    createElement() {
+        console.log(tabSection);
+        tabSection.insertAdjacentHTML('beforeend', '<p style="background-color: '+ this.color +'" data-tabId="' + this.tabId + '" data-itemId="' + this.itemId + '">' + this.name + '</p>');
+    }
 }
 
 
 // Manage Tabs
+const MAX_TABS = 100;
+let tabSection = document.getElementById('tabs');
 let tabs = [];
-let main = document.querySelector('main');
+let tabsId = [];
 
 let addTabButton = document.getElementById('addTab');
 addTabButton.addEventListener('click', function () {
-    console.log('click');
-    tabs.push(new Tab(tabs.length, 'Première tab', '#A1F3E5'));
+    if (tabsId.length < MAX_TABS) {
+        tabs.push(new Tab('Première tab', '#A1F3E5'));
+    }
 });
+
+// Suppression d'une tab -> utilisé filter(obj => obj.id == idRecherché) poru trouver la bonne tab
 
 
 // Manage Items
+const MAX_ITEMS = 100;
+let itemSection = document.getElementById('items');
